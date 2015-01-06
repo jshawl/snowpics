5df6f060baf7fca533daf0ab436ee60e3f537eb6
Jesse Shawl <jesse@jshawl.com>
2015-01-06 09:58:38 -0500

    add some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Snowpic.destroy_all
+Snowpic.create([
+  {image_url:'http://upload.wikimedia.org/wikipedia/commons/b/b2/UK_snow_February_2,_2009_img008.jpg', caption: '2 hour delay yay'},
+  {image_url:'http://apps.startribune.com/blogs/user_images/pauldouglas_1296677775_snowmageddon.jpg', caption: 'Snowmageddon; headed to the thirsty turtle'}
+])
+
+
